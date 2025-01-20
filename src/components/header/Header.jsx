1fd02ba027fc208501a7c1838a510f0a05007859
i.yanchyk <i.yanchyk@steel.mil.ua>
2025-01-20 22:52:50 +0200

    Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,33 +1,33 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
-
 import css from './Header.module.css';
 import logo from '../../img/logo.svg'; // імпортуємо SVG файл
+
+const navigationLinks = [
+  { path: "/", label: "Home" },
+  { path: "/catalog", label: "Catalog" }
+];
+
 const Header = () => {
   return (
-    <>
-      <header>
-        <div className={css.header_wrapper}>
-          <Link to="/">
-            <img src={logo} alt="Logo" className={css.header_logo} />
-          </Link>
-          <nav className={css.header_navigation}>
+    <header>
+      <div className={css.header_wrapper}>
+        <Link to="/">
+          <img src={logo} alt="Logo" className={css.header_logo} />
+        </Link>
+        <nav className={css.header_navigation}>
+          {navigationLinks.map((link) => (
             <NavLink
+              key={link.path}
               className={({ isActive }) => (isActive ? css.active_link : css.navigation_link)}
-              to="/"
+              to={link.path}
             >
-              Home
+              {link.label}
             </NavLink>
-            <NavLink
-              className={({ isActive }) => (isActive ? css.active_link : css.navigation_link)}
-              to="/catalog"
-            >
-              Catalog
-            </NavLink>
-          </nav>
-        </div>
-      </header>
-    </>
+          ))}
+        </nav>
+      </div>
+    </header>
   );
 };
 
