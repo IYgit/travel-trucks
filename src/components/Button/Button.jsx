ac1724416e26202c0e1f9c9f438c2b5d4f6f582b
i.yanchyk <i.yanchyk@steel.mil.ua>
2025-01-20 22:03:57 +0200

    Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,24 @@
-const Button = ({ text, className, onClick, type }) => {
+import PropTypes from 'prop-types';
+
+const Button = ({ text, className, onClick, type = 'button' }) => {
   return (
-    <button type={type || 'button'} onClick={onClick} className={className}>
+    <button type={type} onClick={onClick} className={className}>
       {text}
     </button>
   );
+};
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  onClick: PropTypes.func,
+  type: PropTypes.oneOf(['button', 'submit', 'reset']),
+};
+
+Button.defaultProps = {
+  className: '',
+  onClick: () => {},
+  type: 'button', 
 };
 
 export default Button;
