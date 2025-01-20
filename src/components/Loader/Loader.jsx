526313880d9b41b76cc706784d928f931f0e2f95
i.yanchyk <i.yanchyk@steel.mil.ua>
2025-01-20 23:00:05 +0200

    Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,21 +1,15 @@
 import styled from 'styled-components';
 import css from './Loader.module.css';
 
-const Loader = ({ type }) => {
-  return (
-    <div className={type === 'page loader' ? css.loader : ''}>
-      <StyledWrapper>
-        <div className="loader">
-          <svg
-            xmlSpace="preserve"
-            viewBox="0 0 254.532 254.532"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-            xmlns="http://www.w3.org/2000/svg"
-            id="Capa_1"
-            version="1.1"
-            className="wheel"
-          >
-            <g>
+const LoaderSVG = () => (
+  <svg
+    xmlSpace="preserve"
+    viewBox="0 0 254.532 254.532"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    xmlns="http://www.w3.org/2000/svg"
+    className="wheel"
+  >
+    <g>
               <path
                 d="M127.267,0C57.092,0,0,57.091,0,127.266s57.092,127.266,127.267,127.266c70.174,0,127.266-57.091,127.266-127.266
       				S197.44,0,127.267,0z M127.267,217.656c-49.922,0-90.391-40.468-90.391-90.39s40.469-90.39,90.391-90.39
@@ -44,9 +38,15 @@ const Loader = ({ type }) => {
                 id="rim"
               />
             </g>
-          </svg>
-          <div className="road" />
-        </div>
+  </svg>
+);
+
+const Loader = ({ type = 'page loader' }) => {
+  return (
+    <div className={type === 'page loader' ? css.loader : ''}>
+      <StyledWrapper>
+        <LoaderSVG />
+        <div className="road" />
       </StyledWrapper>
     </div>
   );
@@ -66,14 +66,6 @@ const StyledWrapper = styled.div`
     border-radius: 6em;
     text-align: center;
     animation: rolling 1s linear infinite;
-  }
-
-  #rim {
-    fill: rgb(126, 126, 126);
-  }
-
-  #tire {
-    fill: rgb(55, 55, 55);
   }
 
   .road {
@@ -96,7 +88,6 @@ const StyledWrapper = styled.div`
     from {
       transform: translateX(-140%);
     }
-
     to {
       transform: translateX(100%);
     }
